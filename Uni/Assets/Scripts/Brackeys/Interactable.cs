c8c5d65a30d3551ba0a5d8424fbd114561c7a0ba
tan jun rong <tanjunrong321@gmail.com>
2019-04-05 18:16:01 +0800

    Added base game

---
+++ Uni/Assets/Scripts/Brackeys/Interactable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/*	
+	This component is for all objects that the player can
+	interact with such as enemies, items etc. It is meant
+	to be used as a base class.
+*/
+
+public class Interactable : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Player")) {
+			// Show pick up ui (X to pick up)
+		}
+	}
+
+}
---
+++ Uni/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimationController : Singleton<PlayerAnimationController> {
+
+	private Animator anim;
+
+	private void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	public void Walk(bool state) {
+		anim.SetBool("Walk", state);
+	}
+}
